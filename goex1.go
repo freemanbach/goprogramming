984d1733774e7017292893304366009971a3416c
freemanbach <flo@radford.edu>
2018-12-11 23:31:34 -0500

    Added this Hello Planet file

---
+++ goex1.go
@@ -0,0 +1,18 @@
+package main
+
+/**
+* @author  Freeman
+* @version 0.0.1 
+* @link https://golang.org/pkg/
+* @since   2018-11-1
+*/
+
+
+import "fmt"
+
+
+func main() {
+
+	fmt.Print("Hello Planet, Earth !")
+	fmt.Print("\n")
+}
