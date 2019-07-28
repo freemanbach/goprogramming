bdc678982fd8808cb8280a976256b879119dcb1b
Freeman <flo@radford.edu>
2019-07-28 18:45:21 -0400

    Fixed spacing

--- examples/goex7.go
+++ examples/goex7.go
@@ -18,7 +18,6 @@ import (
 	"bufio"
 	"syscall"
 	"strconv"
-//	"unicode" unicode.isDigit()
 	"math/rand"
 )
 
@@ -28,50 +27,49 @@ func isDigit(mess string) bool {
     return err == nil
 }
 
-// quick and dirty conversion
+// random number range
 func randInt(min int, max int) int {
     return min + rand.Intn(max-min)
 }
 
 func playgame() {
 
-	scan := bufio.NewScanner(os.Stdin)
-	fmt.Print("Want to play this game: (y or n) ? ")
-	scan.Scan()
-	play := scan.Text()
+    scan := bufio.NewScanner(os.Stdin)
+    fmt.Print("Want to play this game: (y or n) ? ")
+    scan.Scan()
+    play := scan.Text()
 
     for play == "y" || play == "yes"  {
-		fmt.Println()
-		fmt.Print("Enter in a value inclusive (0 - 10) ? ")
-		scan.Scan()
-		temp := scan.Text()
-		if isDigit(temp) == true  {
-			num, err := strconv.ParseInt(temp, 10, 64)
-			if err != nil {
-				fmt.Println(err)
-			}
-			num1 := int(num)
-			guessMyNum(num1)
-			fmt.Println()
-			fmt.Print("Want to play: (y or n) ? ")
-			scan.Scan()
-			play := scan.Text()
-			if ( play == "n" || play == "no") {
-				fmt.Println("Exiting Game.....")
-				syscall.Exit(0)
-			} else {
-				play = "y"
-			}
-		} else {
-			syscall.Exit(0)
-		}
-	}
+        fmt.Println()
+        fmt.Print("Enter in a value inclusive (0 - 10) ? ")
+        scan.Scan()
+        temp := scan.Text()
+        if isDigit(temp) == true  {
+            num, err := strconv.ParseInt(temp, 10, 64)
+            if err != nil {
+                fmt.Println(err)
+            }
+            num1 := int(num)
+            guessMyNum(num1)
+            fmt.Println()
+	    fmt.Print("Want to play: (y or n) ? ")
+	    scan.Scan()
+	    play := scan.Text()
+	    if ( play == "n" || play == "no") {
+		    fmt.Println("Exiting Game.....")
+		    syscall.Exit(0)
+            } else {
+		    play = "y"
+              }
+         } else {
+		syscall.Exit(0)
+         }
+    }
 }
 
 func guessMyNum(num int) {
-
-	rand.Seed(time.Now().UnixNano())
-	// rand.Seed(ns.Nanoseconds())
+    rand.Seed(time.Now().UnixNano())
+    // rand.Seed(ns.Nanoseconds())
     pc := randInt(1, 11)
 
     if num < pc {
@@ -79,10 +77,10 @@ func guessMyNum(num int) {
     } else if num > pc {
         fmt.Println("Your guess is higher than PC Number. ", "PC Number: ", pc)
     } else if pc == num {
-	    fmt.Println("You have guessed correctly.")
+	fmt.Println("You have guessed correctly.")
     } else {
-	    fmt.Println("Error: something is incorrect.")
-	    syscall.Exit(1)
+	fmt.Println("Error: something is incorrect.")
+	syscall.Exit(1)
     }
 }
 
